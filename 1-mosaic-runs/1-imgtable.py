831c6a7f142045c4a0616d5c0db439b968d9e0a3
Geert Barentsen <geert@barentsen.be>
2012-10-31 15:24:55 +0000

    Initial commit

---
+++ 1-mosaic-runs/1-imgtable.py
@@ -0,0 +1,143 @@
+"""
+Walk through the IPHAS raw data directory 
+and produce a CSV file with the location of all images and confidence maps
+"""
+import os
+import re
+import logging
+import pyfits
+import numpy as np
+
+"""CONFIGURATION"""
+
+# Initialize logging
+logging.basicConfig( level=logging.WARNING )
+
+# Where are the images?
+datadir = "/media/0133d764-0bfe-4007-a9cc-a7b1f61c4d1d/iphas/"
+
+# Initialize output table
+out = open('iphas-images.csv', 'w')
+out.write('run,field,filter,image,confmap\n')
+
+# Confidence maps can have various different filenames
+CONF_NAMES = {'ha': ['Ha_conf.fits', 'Ha_conf.fit', 
+                    'Halpha_conf.fit',
+                    'ha_conf.fits', 'ha_conf.fit', 
+                    'h_conf.fits', 'h_conf.fit',
+                    'Halpha:197_iphas_aug2003_cpm.fit', 
+                    'Halpha:197_iphas_sep2003_cpm.fit',
+                    'Halpha:197_iphas_oct2003_cpm.fit', 
+                    'Halpha:197_iphas_nov2003_cpm.fit', 
+                    'Halpha:197_nov2003b_cpm.fit', 
+                    'Halpha:197_dec2003_cpm.fit',
+                    'Halpha:197_jun2004_cpm.fit', 
+                    'Halpha:197_iphas_jul2004a_cpm.fit',
+                    'Halpha:197_iphas_jul2004_cpm.fit', 
+                    'Halpha:197_iphas_aug2004a_cpm.fit',
+                    'Halpha:197_iphas_aug2004b_cpm.fit', 
+                    'Halpha:197_iphas_dec2004b_cpm.fit'], 
+                'r': ['r_conf.fit', 'r_conf.fits', 
+                    'r:214_iphas_aug2003_cpm.fit', 
+                    'r:214_dec2003_cpm.fit', 
+                    'r:214_iphas_nov2003_cpm.fit', 
+                    'r:214_nov2003b_cpm.fit', 
+                    'r:214_iphas_sep2003_cpm.fit',
+                    'r:214_iphas_aug2004a_cpm.fit', 
+                    'r:214_iphas_aug2004b_cpm.fit',
+                    'r:214_iphas_jul2004a_cpm.fit', 
+                    'r:214_iphas_jul2004_cpm.fit',
+                    'r:214_jun2004_cpm.fit'],
+                'i': ['i_conf.fit', 'i_conf.fits', 
+                    'i:215_iphas_aug2003_cpm.fit', 
+                    'i:215_dec2003_cpm.fit', 
+                    'i:215_iphas_nov2003_cpm.fit', 
+                    'i:215_nov2003b_cpm.fit', 
+                    'i:215_iphas_sep2003_cpm.fit',
+                    'i:215_iphas_aug2004a_cpm.fit', 
+                    'i:215_iphas_aug2004b_cpm.fit',
+                    'i:215_iphas_jul2004a_cpm.fit', 
+                    'i:215_iphas_jul2004_cpm.fit',
+                    'i:215_jun2004_cpm.fit']}
+
+
+
+# Dict to hold the confidence maps for each filter/directory
+confmaps = {'r' : {}, 'i' : {}, 'ha' : {}}
+
+def get_confmap(mydir, band):
+    """Return the name of the confidence map in directory 'mydir'"""
+    assert( band in ['r', 'i', 'ha'] )
+
+    # The result from previous function calls are stored in 'confmaps'
+    if mydir not in confmaps[band].keys():
+
+        # Some directories do not contain confidence maps
+        if mydir == datadir+'iphas_nov2006c':
+            candidatedir = datadir+'iphas_nov2006b'
+        elif mydir == datadir+'iphas_jul2008':
+            candidatedir = datadir+'iphas_aug2008'
+        elif mydir == datadir+'iphas_oct2009':
+            candidatedir = datadir+'iphas_nov2009'       
+        elif mydir == datadir+'run10':
+            candidatedir = datadir+'run11'
+        elif mydir == datadir+'run13':
+            candidatedir = datadir+'run12'
+        else:
+            candidatedir = mydir
+
+        # Try all possible names
+        for name in CONF_NAMES[band]: 
+            candidate = candidatedir+'/'+name
+            if os.path.exists(candidate):
+                confmaps[band][mydir] = candidate # Success!
+                continue
+
+    # Return confidence map name if we found one, raise exception otherwise
+    try:
+        return confmaps[band][mydir]
+    except KeyError:
+        raise Exception('No confidence map found in directory %s' % mydir)
+
+
+# Load file containing and IPHAS metadata (to figure out field ids)
+metadata = pyfits.getdata('/home/gb/dev/iphas-qc/data/iphas-observations.fits', 1)
+
+def get_fieldid(myrun):
+    """Retrieve an IPHAS field identifier of the form 'fieldnumber_month_filter'"""
+    run_int = int(myrun[1:]) # Numeric run number
+    # Check the iphas-observations.fits file for columns "run_r", "run_i", "run_ha"
+    for myfilter in ['r', 'i', 'ha']:
+        a = np.argwhere( metadata.field('run_'+myfilter) == run_int )
+        if len(a) > 0:
+            # If the run number matches, return field identifier+filter
+            return metadata.field('id')[a[0]][0]+","+myfilter
+    # No match: return empty string
+    return ","
+
+
+
+""" MAIN """
+# The raw data dir contains 'special' sub-directories with exposures to ignore
+directories_to_ignore = ['junk', 'badones', 'crap', '9thoct', \
+                         'Uband', 'gband', 'slow']
+
+for mydir in os.walk(datadir):
+    # We're ignoring certain directories
+    if mydir[0].split('/')[-1] in directories_to_ignore:
+        continue
+
+    # Consider each file
+    for filename in mydir[2]:
+        # Images should be named "rnnnnnn.fit"
+        if re.match('^r\d+.fit', filename):
+            logging.debug("%s/%s" % (mydir[0], filename))
+            image_path = os.path.join(mydir[0], filename)
+            conf_path = get_confmap(mydir[0], 'i')
+            # Run number is the first part of the filename
+            myrun = filename.split('.')[0]
+            # Write the details of this image
+            out.write("%s,%s,%s,%s\n" % \
+                     (myrun, get_fieldid(myrun), image_path[len(datadir):], conf_path[len(datadir):])) 
+
+out.close()
---
+++ 1-mosaic-runs/2-mosaic-mpi.py
@@ -0,0 +1,116 @@
+"""
+Execute a list of commands using MPI
+e.g. mpirun -np 4 python do-mosaic-mpi.py
+"""
+from mpi4py import MPI
+import logging
+import subprocess
+import shlex
+import os
+
+# Setup MPI and logging
+comm = MPI.COMM_WORLD
+logging.basicConfig(level=logging.DEBUG, 
+    format="%(asctime)s/W"+str(comm.rank)+"/%(levelname)s: %(message)s", 
+    datefmt="%Y-%m-%d %H:%M:%S" )
+
+# Where are the input images?
+in_dir = '/media/0133d764-0bfe-4007-a9cc-a7b1f61c4d1d/iphas'
+# Where to write the output?
+out_dir = '/tmp/iphas-mosaic'
+# Mosaicking command
+mosaic_cmd = '/home/gb/bin/casutools/bin/mosaic'
+# fpack command (-D deletes original file, -Y suppresses warning)
+fpack_cmd = '/home/gb/bin/cfitsio3310/bin/fpack -D -Y'
+
+# Define the messages we'll be passing through MPI
+GIVE_ME_WORK = 801  # Worker waiting for instructions
+FINISHED = 850      # All work is done
+
+
+
+def mpi_run():
+    """Figure out whether we are master or worker"""
+    if comm.rank==0:
+        mpi_master()
+    else:
+        mpi_worker()
+    return
+
+def mpi_master():
+    logging.info("Running on %d cores" % comm.size)
+
+    images = open('iphas-images.csv', 'r')
+    rows = images.readlines()
+    # Ignore the first line of the CSV table (=header)
+    for i, row in enumerate(rows[1:]):
+        cols = row.strip().split(',')
+        if cols[1] == "":
+            continue
+
+        # Wait for a worker to report for duty
+        rank_done = comm.recv(source=MPI.ANY_SOURCE, tag=GIVE_ME_WORK)
+        # Send the worker the details of the next image
+        msg = {'field':cols[1], 'filter':cols[2], 'img':cols[3], 'conf':cols[4]}
+        comm.send(msg, dest=rank_done)
+        logging.info('Image %d/%d sent to worker %s' % (i, len(rows), rank_done))
+
+
+    # Tell all workers we're finished
+    for worker in range(1, comm.size):
+        comm.send(FINISHED, dest=worker)
+    
+    return
+
+
+def cmd_exec(cmd):
+    """Execute a shell command"""
+    logging.debug(cmd)
+    p = subprocess.Popen(shlex.split(cmd), 
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout = p.stdout.read().strip()
+    stderr = p.stderr.read().strip()
+    if stderr:
+        logging.error("Error output detected! STDERR={%S} STDOUT={%s} CMD={%s}" % (stderr, stdout, cmd))
+        raise Exception('Command failed: %s' % cmd)
+    logging.debug( stdout )
+
+
+def mpi_worker():
+    while True:
+        # Ask for work
+        comm.send(comm.rank, dest=0, tag=GIVE_ME_WORK)
+        msg = comm.recv(source=0)
+        if msg == FINISHED:
+            return
+
+        # Perform work
+        logging.debug("Message rcvd: \"%s\"" % msg)
+
+        # Full paths of input/output images and confidence maps
+        in_img = "%s/%s" % (in_dir, msg['img'])
+        in_conf = "%s/%s" % (in_dir, msg['conf'])
+        out_img = "%s/%s_%s_mosaic.fit" % (out_dir, msg['field'], msg['filter'])
+        out_conf = "%s/%s_%s_conf.fit" % (out_dir, msg['field'], msg['filter'])
+        
+
+        commands = []
+        # Casutools/mosaic command
+        commands.append( "%s %s %s %s %s --skyflag=0 --verbose" \
+                % (mosaic_cmd, in_img, in_conf, out_img, out_conf) )
+        # Compression using fpack
+        for filename in [out_img, out_conf]:
+            if os.path.exists(filename+".fz"):
+                commands.append( "rm %s.fz" % filename )
+            commands.append( "%s %s" % (fpack_cmd, filename) )
+
+        # Execute!
+        try:
+            for cmd in commands:
+                cmd_exec(cmd)
+        except:
+            logging.error( "Aborted %s_%s" % (msg['field'], msg['filter']) )
+
+
+""" MAIN """
+mpi_run()
